349afdf5268e7392255cc86df75feeb96c3ebd42
Saswat Padhi <saswat.sourav@gmail.com>
2020-09-20 00:26:18 +0000

    Update r40.setup-network.sh

--- setup.d/r40.setup-network.sh
+++ setup.d/r40.setup-network.sh
@@ -4,7 +4,7 @@ set -Eeumox pipefail
 
 hostname
 
-read -p "Please enter the new hostname: " NEW_HOSTNAME
+read NEW_HOSTNAME
 
 hostnamectl set-hostname $NEW_HOSTNAME
 echo '127.0.0.1 $NEW_HOSTNAME' >> /etc/hosts
