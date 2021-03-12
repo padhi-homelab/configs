71566faaa0bac2c541e410dfdbfb062aa1f986fb
Saswat Padhi <saswat.sourav@gmail.com>
2021-03-12 00:00:16 -0500

    minor

--- setup.d/u00.copy-home-files.sh
+++ setup.d/u00.copy-home-files.sh
@@ -6,6 +6,6 @@ SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 cd "${SELF_DIR}"/../home
 
-rsync -avhu --progress . /home/user
+rsync -avhu --progress . ${HOME}
 
-chmod +x /home/user/backup.sh
+chmod +x ${HOME}/backup.sh
--- setup.d/u20.configure-backup.sh
+++ setup.d/u20.configure-backup.sh
@@ -10,3 +10,8 @@ systemctl --user daemon-reload
 systemctl --user enable backup.timer
 systemctl --user start backup.timer
 systemctl --user status backup.timer
+
+# systemd timers run only when user is logged in,
+# so we need to enable lingering:
+# https://wiki.archlinux.org/index.php/Systemd/User#Automatic_start-up_of_systemd_user_instances
+loginctl enable-linger ${USER}
