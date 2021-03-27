7c1a0479a387bdda8b40ca942ecbb81c6464edb9
Saswat Padhi <saswat.sourav@gmail.com>
2021-03-26 20:11:35 -0400

    minor fix to backup script

--- home/backup.sh
+++ home/backup.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 TIME="$(date +%Y%m%d%H%M%S)"
-FILENAME=$TIME.tar
+PREFIX=$HOSTNAME-backup-
 
-SRCS="/home/user/services /home/user/.config/systemd /home/user/backup.sh"
+SRCS=("/home/user/services" "/home/user/.config/systemd" "/home/user/backup.sh")
 
-tar --exclude-vcs -cf "$HOME/$FILENAME" $SRCS
+FILENAME=$PREFIX$TIME.tar
+tar --exclude-vcs -cf "$HOME/$FILENAME" "${SRCS[@]}"
 
 DESTDIR=/backup
 cd "$DESTDIR"
 
 MAX_BACKUPS_TO_KEEP=32
-ls -1tr "$DESTDIR"/*.tar | head -n -$MAX_BACKUPS_TO_KEEP | while read -r i
+ls -1tr "$DESTDIR/$PREFIX"*.tar | head -n -$MAX_BACKUPS_TO_KEEP | while read -r i
 do
   rm "$i"
 done
 
 mv "$HOME/$FILENAME" .
+rm "$HOME/$PREFIX"*.tar
